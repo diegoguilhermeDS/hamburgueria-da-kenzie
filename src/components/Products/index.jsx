08ad7c0414cb54be2a7a31076a18dba859e961c3
Diego Guilherme <diegoguilherme752@oultook.com>
2022-11-23 19:32:47 -0300

    feat: component products list

---
+++ src/components/Products/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <li>
+        <img src="" alt="imagem do produto" />
+        <div>
+            <h3>Hamburguer</h3>
+            <small>Sanduíches</small>
+            <span>R$ 14.00</span>
+            <button>Adicionar</button>
+        </div>
+    </li>
+  )
+}
+
+export default Products
