4594ad5a6950dd7c9fac142628470e2f8aa29f6e
Diego Guilherme <diegoguilherme752@oultook.com>
2022-11-27 14:08:47 -0300

    feat: preload

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import { useEffect, useState } from "react";
 import { api } from "./services/api";
 import { GlobalStyle } from "./styles/global";
 import { Container } from "./styles/Container";
+import PreLoad from "./components/PreLoad";
 
 function App() {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [currentSale, setCurrentSale] = useState([]);
+  const [preloadState, setPreloadState] = useState(true);
 
   useEffect(() => {
     async function getProducts() {
       try {
         const response = await api.get("");
         setProducts(response.data);
-        response.data.map((dat) => dat.amount = 1)
+        response.data.map((dat) => (dat.amount = 1));
         setFilteredProducts(response.data);
       } catch (error) {
         console.log(error);
@@ -28,22 +30,27 @@ function App() {
 
   return (
     <div className="App">
-      <GlobalStyle/>
-      <Header
-        filteredProducts={filteredProducts}
-        setFilteredProducts={setFilteredProducts}
-        products={products}
-      />
-      <Container>
-        <section>
-          <ProductsList
-            products={filteredProducts}
-            currentSale={currentSale}
-            setCurrentSale={setCurrentSale}
+      <GlobalStyle />
+      {preloadState && <PreLoad setPreloadState={setPreloadState} />}
+      {!preloadState && (
+        <div className="content">
+          <Header
+            filteredProducts={filteredProducts}
+            setFilteredProducts={setFilteredProducts}
+            products={products}
           />
-        </section>
-        <Cart currentSale={currentSale} setCurrentSale={setCurrentSale} />
-      </Container>
+          <Container>
+            <section>
+              <ProductsList
+                products={filteredProducts}
+                currentSale={currentSale}
+                setCurrentSale={setCurrentSale}
+              />
+            </section>
+            <Cart currentSale={currentSale} setCurrentSale={setCurrentSale} />
+          </Container>
+        </div>
+      )}
     </div>
   );
 }
